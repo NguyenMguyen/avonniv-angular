83e0894adb20f8670ba61353f14c3d91da3755c9
Trần Đình Nguyên <nguyentran9122000@gmail.com>
2022-05-17 11:15:21 +0700

    done about-us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutYmnerComponent } from './about-ymner/about-ymner.component';
+import { FindGrantsComponent } from './find-grants/find-grants.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     AboutYmnerComponent,
+    FindGrantsComponent,
   ],
   imports: [
     BrowserModule,
